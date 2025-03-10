f51b7582cc0e551e29d21461d158004b47406fed
James211902 <james211902@gmail.com>
2025-03-10 18:45:34 +0000

    - Added ability to swap dlls

--- X4DLSSModifier/MainForm.Designer.cs
+++ X4DLSSModifier/MainForm.Designer.cs
@@ -45,29 +45,41 @@
             label6 = new Label();
             cmbDlssPerformancePreset = new ComboBox();
             nudDlssPerformancePresetValue = new NumericUpDown();
+            groupBox4 = new GroupBox();
+            label8 = new Label();
+            cmbDLSSAllPreset = new ComboBox();
+            groupBox5 = new GroupBox();
+            cmbDlssDll = new ComboBox();
+            label7 = new Label();
+            groupBox6 = new GroupBox();
+            linkLabel1 = new LinkLabel();
             ((System.ComponentModel.ISupportInitialize)nudDlssQualityPresetValue).BeginInit();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)nudDlssBalancedPresetValue).BeginInit();
             groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)nudDlssPerformancePresetValue).BeginInit();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox6.SuspendLayout();
             SuspendLayout();
             // 
             // cmbDlssQualityPreset
             // 
+            cmbDlssQualityPreset.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             cmbDlssQualityPreset.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbDlssQualityPreset.FormattingEnabled = true;
             cmbDlssQualityPreset.Items.AddRange(new object[] { "Default", "A", "B", "C", "D", "E", "F", "J", "K", "Custom" });
             cmbDlssQualityPreset.Location = new Point(80, 16);
             cmbDlssQualityPreset.Name = "cmbDlssQualityPreset";
-            cmbDlssQualityPreset.Size = new Size(87, 23);
+            cmbDlssQualityPreset.Size = new Size(89, 23);
             cmbDlssQualityPreset.TabIndex = 0;
             cmbDlssQualityPreset.SelectedIndexChanged += cmbDlssQualityPreset_SelectedIndexChanged;
             // 
             // chkDlssIndicator
             // 
             chkDlssIndicator.AutoSize = true;
-            chkDlssIndicator.Location = new Point(12, 264);
+            chkDlssIndicator.Location = new Point(10, 22);
             chkDlssIndicator.Name = "chkDlssIndicator";
             chkDlssIndicator.Size = new Size(140, 19);
             chkDlssIndicator.TabIndex = 1;
@@ -76,20 +88,21 @@
             // 
             // btnApply
             // 
-            btnApply.Location = new Point(112, 289);
+            btnApply.Location = new Point(335, 286);
             btnApply.Name = "btnApply";
             btnApply.Size = new Size(75, 23);
             btnApply.TabIndex = 2;
-            btnApply.Text = "Apply";
+            btnApply.Text = "Apply Presets";
             btnApply.UseVisualStyleBackColor = true;
             btnApply.Click += btnApply_Click;
             // 
             // nudDlssQualityPresetValue
             // 
+            nudDlssQualityPresetValue.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             nudDlssQualityPresetValue.Location = new Point(111, 45);
             nudDlssQualityPresetValue.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
             nudDlssQualityPresetValue.Name = "nudDlssQualityPresetValue";
-            nudDlssQualityPresetValue.Size = new Size(56, 23);
+            nudDlssQualityPresetValue.Size = new Size(58, 23);
             nudDlssQualityPresetValue.TabIndex = 3;
             nudDlssQualityPresetValue.ValueChanged += nudDlssQualityPresetValue_ValueChanged;
             // 
@@ -117,9 +130,9 @@
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(cmbDlssQualityPreset);
             groupBox1.Controls.Add(nudDlssQualityPresetValue);
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Location = new Point(6, 51);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(175, 78);
+            groupBox1.Size = new Size(177, 78);
             groupBox1.TabIndex = 6;
             groupBox1.TabStop = false;
             groupBox1.Text = "Quality";
@@ -130,9 +143,9 @@
             groupBox2.Controls.Add(label4);
             groupBox2.Controls.Add(cmbDlssBalancedPreset);
             groupBox2.Controls.Add(nudDlssBalancedPresetValue);
-            groupBox2.Location = new Point(12, 96);
+            groupBox2.Location = new Point(6, 135);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(175, 78);
+            groupBox2.Size = new Size(177, 78);
             groupBox2.TabIndex = 7;
             groupBox2.TabStop = false;
             groupBox2.Text = "Balanced";
@@ -157,21 +170,23 @@
             // 
             // cmbDlssBalancedPreset
             // 
+            cmbDlssBalancedPreset.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             cmbDlssBalancedPreset.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbDlssBalancedPreset.FormattingEnabled = true;
             cmbDlssBalancedPreset.Items.AddRange(new object[] { "Default", "A", "B", "C", "D", "E", "F", "J", "K", "Custom" });
             cmbDlssBalancedPreset.Location = new Point(80, 16);
             cmbDlssBalancedPreset.Name = "cmbDlssBalancedPreset";
-            cmbDlssBalancedPreset.Size = new Size(87, 23);
+            cmbDlssBalancedPreset.Size = new Size(89, 23);
             cmbDlssBalancedPreset.TabIndex = 0;
             cmbDlssBalancedPreset.SelectedIndexChanged += cmbDlssBalancedPreset_SelectedIndexChanged;
             // 
             // nudDlssBalancedPresetValue
             // 
+            nudDlssBalancedPresetValue.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             nudDlssBalancedPresetValue.Location = new Point(111, 45);
             nudDlssBalancedPresetValue.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
             nudDlssBalancedPresetValue.Name = "nudDlssBalancedPresetValue";
-            nudDlssBalancedPresetValue.Size = new Size(56, 23);
+            nudDlssBalancedPresetValue.Size = new Size(58, 23);
             nudDlssBalancedPresetValue.TabIndex = 3;
             nudDlssBalancedPresetValue.ValueChanged += nudDlssBalancedPresetValue_ValueChanged;
             // 
@@ -181,9 +196,9 @@
             groupBox3.Controls.Add(label6);
             groupBox3.Controls.Add(cmbDlssPerformancePreset);
             groupBox3.Controls.Add(nudDlssPerformancePresetValue);
-            groupBox3.Location = new Point(12, 180);
+            groupBox3.Location = new Point(6, 219);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(175, 78);
+            groupBox3.Size = new Size(177, 78);
             groupBox3.TabIndex = 7;
             groupBox3.TabStop = false;
             groupBox3.Text = "Performance";
@@ -208,37 +223,124 @@
             // 
             // cmbDlssPerformancePreset
             // 
+            cmbDlssPerformancePreset.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             cmbDlssPerformancePreset.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbDlssPerformancePreset.FormattingEnabled = true;
             cmbDlssPerformancePreset.Items.AddRange(new object[] { "Default", "A", "B", "C", "D", "E", "F", "J", "K", "Custom" });
             cmbDlssPerformancePreset.Location = new Point(80, 16);
             cmbDlssPerformancePreset.Name = "cmbDlssPerformancePreset";
-            cmbDlssPerformancePreset.Size = new Size(87, 23);
+            cmbDlssPerformancePreset.Size = new Size(89, 23);
             cmbDlssPerformancePreset.TabIndex = 0;
             cmbDlssPerformancePreset.SelectedIndexChanged += cmbDlssPerformancePreset_SelectedIndexChanged;
             // 
             // nudDlssPerformancePresetValue
             // 
+            nudDlssPerformancePresetValue.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             nudDlssPerformancePresetValue.Location = new Point(111, 45);
             nudDlssPerformancePresetValue.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
             nudDlssPerformancePresetValue.Name = "nudDlssPerformancePresetValue";
-            nudDlssPerformancePresetValue.Size = new Size(56, 23);
+            nudDlssPerformancePresetValue.Size = new Size(58, 23);
             nudDlssPerformancePresetValue.TabIndex = 3;
             nudDlssPerformancePresetValue.ValueChanged += nudDlssPerformancePresetValue_ValueChanged;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label8);
+            groupBox4.Controls.Add(groupBox1);
+            groupBox4.Controls.Add(cmbDLSSAllPreset);
+            groupBox4.Controls.Add(groupBox3);
+            groupBox4.Controls.Add(groupBox2);
+            groupBox4.Location = new Point(12, 12);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(189, 304);
+            groupBox4.TabIndex = 8;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "DLSS Presets";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 25);
+            label8.Name = "label8";
+            label8.Size = new Size(40, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Set All";
+            // 
+            // cmbDLSSAllPreset
+            // 
+            cmbDLSSAllPreset.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            cmbDLSSAllPreset.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDLSSAllPreset.FormattingEnabled = true;
+            cmbDLSSAllPreset.Items.AddRange(new object[] { "Default", "A", "B", "C", "D", "E", "F", "J", "K" });
+            cmbDLSSAllPreset.Location = new Point(58, 22);
+            cmbDLSSAllPreset.Name = "cmbDLSSAllPreset";
+            cmbDLSSAllPreset.Size = new Size(117, 23);
+            cmbDLSSAllPreset.TabIndex = 6;
+            cmbDLSSAllPreset.SelectedIndexChanged += cmbDLSSAllPreset_SelectedIndexChanged;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(cmbDlssDll);
+            groupBox5.Controls.Add(label7);
+            groupBox5.Location = new Point(207, 12);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(200, 61);
+            groupBox5.TabIndex = 9;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "DLSS DLL";
+            // 
+            // cmbDlssDll
+            // 
+            cmbDlssDll.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDlssDll.FormattingEnabled = true;
+            cmbDlssDll.Location = new Point(84, 27);
+            cmbDlssDll.Name = "cmbDlssDll";
+            cmbDlssDll.Size = new Size(110, 23);
+            cmbDlssDll.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(10, 30);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 15);
+            label7.TabIndex = 0;
+            label7.Text = "DLL Version";
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(linkLabel1);
+            groupBox6.Controls.Add(chkDlssIndicator);
+            groupBox6.Location = new Point(207, 81);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(200, 77);
+            groupBox6.TabIndex = 10;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Debug Settings";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(6, 44);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(130, 15);
+            linkLabel1.TabIndex = 2;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "What is DLSS Indicator?";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(199, 321);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
+            ClientSize = new Size(422, 321);
+            Controls.Add(groupBox6);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
             Controls.Add(btnApply);
-            Controls.Add(chkDlssIndicator);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
-            MaximumSize = new Size(215, 360);
-            MinimumSize = new Size(215, 360);
+            MaximumSize = new Size(438, 360);
+            MinimumSize = new Size(438, 360);
             Name = "MainForm";
             Text = "X4 DLSS Modifier";
             Load += MainForm_Load;
@@ -251,8 +353,13 @@
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)nudDlssPerformancePresetValue).EndInit();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -274,5 +381,13 @@
         private Label label6;
         private ComboBox cmbDlssPerformancePreset;
         private NumericUpDown nudDlssPerformancePresetValue;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private ComboBox cmbDlssDll;
+        private Label label7;
+        private GroupBox groupBox6;
+        private Label label8;
+        private ComboBox cmbDLSSAllPreset;
+        private LinkLabel linkLabel1;
     }
 }
--- X4DLSSModifier/MainForm.cs
+++ X4DLSSModifier/MainForm.cs
@@ -1,5 +1,6 @@
 using FilePatching;
 using Microsoft.Win32;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace X4DLSSModifier
@@ -10,6 +11,8 @@ namespace X4DLSSModifier
         private PatchHandler _PatchHandler;
         private PatternPatch _Patch;
         private bool _DLSSIndicatorAvailable;
+        private string _WorkingDirectory => Path.GetDirectoryName(_PatchHandler.FilePath);
+
         public MainForm()
         {
             InitializeComponent();
@@ -34,8 +37,8 @@ namespace X4DLSSModifier
                     {
                         newRegValue = regValue & ~0x400;
                     }
-                    if(newRegValue != regValue)
-                        Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\NVIDIA Corporation\\Global\\NGXCore", "ShowDlssIndicator", regValue, RegistryValueKind.DWord);
+                    if (newRegValue != regValue)
+                        Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\NVIDIA Corporation\\Global\\NGXCore", "ShowDlssIndicator", newRegValue, RegistryValueKind.DWord);
                 }
                 var patchResult = _PatchHandler.PatchWith(_Patch, true);
                 if (((int)patchResult != (int)PatchResult.PATCH_SUCCESSFUL))
@@ -44,6 +47,9 @@ namespace X4DLSSModifier
                     throw new Exception("Failed to apply patch. Error Code: " + errorCode);
                 }
                 _PatchHandler.WriteChanges(false);
+
+                SetDLL();
+
                 MessageBox.Show("Patch was applied.", "Success");
             }
             catch (Exception ex)
@@ -75,15 +81,16 @@ namespace X4DLSSModifier
                 ((int)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\NVIDIA Corporation\\Global\\NGXCore", "Installed", 0) == 1);
 
             chkDlssIndicator.Enabled = _DLSSIndicatorAvailable;
-            cmbDlssQualityPreset.SelectedIndex =
-                cmbDlssBalancedPreset.SelectedIndex =
-                cmbDlssPerformancePreset.SelectedIndex = 3;
+            cmbDLSSAllPreset.SelectedIndex = 3;
+
+            ReloadDLLInfo();
 
             // Set to foreground because without it the window is shy and hides behind everything.
             SetForegroundWindow(this.Handle);
         }
 
-        private static int _GetDLSSValue(string value)
+        #region DLSS Presets
+        private static int _GetDLSSPresetValue(string value)
         {
             switch (value)
             {
@@ -109,16 +116,33 @@ namespace X4DLSSModifier
             throw new ArgumentException();
         }
 
+        private bool _SetAllPreset = true;
+        private void cmbDLSSAllPreset_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbDLSSAllPreset.SelectedIndex != -1)
+            {
+                _SetAllPreset = false;
+                cmbDlssQualityPreset.SelectedIndex =
+                    cmbDlssBalancedPreset.SelectedIndex =
+                    cmbDlssPerformancePreset.SelectedIndex = cmbDLSSAllPreset.SelectedIndex;
+                _SetAllPreset = true;
+            }
+        }
+
         #region Quality
         private void cmbDlssQualityPreset_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (_SetAllPreset)
+            {
+                cmbDLSSAllPreset.SelectedIndex = -1;
+            }
             if (cmbDlssQualityPreset.SelectedItem == "Custom")
             {
                 nudDlssQualityPresetValue.Enabled = true;
             }
             else
             {
-                nudDlssQualityPresetValue.Value = _GetDLSSValue(cmbDlssQualityPreset.SelectedItem.ToString());
+                nudDlssQualityPresetValue.Value = _GetDLSSPresetValue(cmbDlssQualityPreset.SelectedItem.ToString());
                 nudDlssQualityPresetValue.Enabled = false;
             }
         }
@@ -130,13 +154,17 @@ namespace X4DLSSModifier
         #region Balanced
         private void cmbDlssBalancedPreset_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (_SetAllPreset)
+            {
+                cmbDLSSAllPreset.SelectedIndex = -1;
+            }
             if (cmbDlssBalancedPreset.SelectedItem == "Custom")
             {
                 nudDlssBalancedPresetValue.Enabled = true;
             }
             else
             {
-                nudDlssBalancedPresetValue.Value = _GetDLSSValue(cmbDlssBalancedPreset.SelectedItem.ToString());
+                nudDlssBalancedPresetValue.Value = _GetDLSSPresetValue(cmbDlssBalancedPreset.SelectedItem.ToString());
                 nudDlssBalancedPresetValue.Enabled = false;
             }
         }
@@ -146,22 +174,146 @@ namespace X4DLSSModifier
         }
         #endregion
         #region Performance
-        private void nudDlssPerformancePresetValue_ValueChanged(object sender, EventArgs e)
-        {
-            _Patch.SetParameter("DLSSPerformancePreset", (byte)nudDlssPerformancePresetValue.Value);
-        }
         private void cmbDlssPerformancePreset_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(cmbDlssPerformancePreset.SelectedItem == "Custom")
+            if (_SetAllPreset)
+            {
+                cmbDLSSAllPreset.SelectedIndex = -1;
+            }
+            if (cmbDlssPerformancePreset.SelectedItem == "Custom")
             {
                 nudDlssPerformancePresetValue.Enabled = true;
             }
             else
             {
-                nudDlssPerformancePresetValue.Value = _GetDLSSValue(cmbDlssPerformancePreset.SelectedItem.ToString());
+                nudDlssPerformancePresetValue.Value = _GetDLSSPresetValue(cmbDlssPerformancePreset.SelectedItem.ToString());
                 nudDlssPerformancePresetValue.Enabled = false;
             }
         }
+        private void nudDlssPerformancePresetValue_ValueChanged(object sender, EventArgs e)
+        {
+            _Patch.SetParameter("DLSSPerformancePreset", (byte)nudDlssPerformancePresetValue.Value);
+        }
         #endregion
+        #endregion
+
+        #region DLSS DLL
+        private class DLSSDLLInfo : IComparable, IEquatable<DLSSDLLInfo>
+        {
+            public int Major;
+            public int Minor;
+            public int Build;
+            public int Private;
+
+            public string FilePath;
+            public DLSSDLLInfo(FileVersionInfo fvi, string filePath)
+            {
+                Major = fvi.ProductMajorPart;
+                Minor = fvi.ProductMinorPart;
+                Build = fvi.ProductBuildPart;
+                Private = fvi.ProductPrivatePart;
+                FilePath = filePath;
+            }
+
+            public int CompareTo(object? obj)
+            {
+                var other = (DLSSDLLInfo)(obj);
+
+                if (Major != other.Major)
+                {
+                    return Major.CompareTo(other.Major);
+                }
+                else if (Minor != other.Minor)
+                {
+                    return Minor.CompareTo(other.Minor);
+                }
+                else if (Build != other.Build)
+                {
+                    return Build.CompareTo(other.Build);
+                }
+                else if (Private != other.Private)
+                {
+                    return Private.CompareTo(other.Private);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            public bool Equals(DLSSDLLInfo? other)
+            {
+                return Major == other.Major && Minor == other.Minor && Build == other.Build && Private == other.Private;
+            }
+
+            public override string ToString()
+            {
+                return string.Join(".", Major, Minor, Build, Private);
+            }
+        }
+        private void ReloadDLLInfo()
+        {
+            cmbDlssDll.Items.Clear();
+            List<DLSSDLLInfo> dlssVersions = new List<DLSSDLLInfo>();
+            var currentDllPath = Path.Combine(_WorkingDirectory, "nvngx_dlss.dll");
+            var nvidiaVersionDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "NVIDIA\\NGX\\models\\dlss\\versions");
+            var files = Directory.GetFiles(nvidiaVersionDirectory, "*.bin", SearchOption.AllDirectories);
+
+
+            foreach (var file in files)
+            {
+                var nvidiaBinVersion = new DLSSDLLInfo(FileVersionInfo.GetVersionInfo(file), file);
+                if (!dlssVersions.Contains(nvidiaBinVersion))
+                {
+                    dlssVersions.Add(nvidiaBinVersion);
+                }
+            }
+
+
+            if (File.Exists(currentDllPath))
+            {
+                var currentVersion = new DLSSDLLInfo(FileVersionInfo.GetVersionInfo(currentDllPath), null);
+                if (dlssVersions.Contains(currentVersion))
+                {
+                    dlssVersions.Remove(currentVersion);
+                }
+                dlssVersions.Add(currentVersion);
+                
+                dlssVersions.Sort();
+                dlssVersions.Reverse();
+                cmbDlssDll.Items.AddRange(dlssVersions.ToArray());
+                cmbDlssDll.SelectedItem = currentVersion;
+            }
+            else
+            {
+                dlssVersions.Sort();
+                dlssVersions.Reverse();
+                cmbDlssDll.Items.AddRange(dlssVersions.ToArray());
+                cmbDlssDll.SelectedIndex = -1;
+            }
+        }
+
+        private void SetDLL()
+        {
+            var selectedDLL = (DLSSDLLInfo)cmbDlssDll.SelectedItem;
+
+            if (selectedDLL.FilePath != null)
+            {
+                File.Copy(selectedDLL.FilePath, Path.Combine(_WorkingDirectory, "nvngx_dlss.dll"), true);
+
+                ReloadDLLInfo();
+            }
+        }
+        #endregion
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            const string DLSS_INDICATOR_INFO_LINK = "https://www.pcgamer.com/nvidia-dlss-indicator/";
+            Process.Start(new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = DLSS_INDICATOR_INFO_LINK
+            });
+        }
     }
 }
